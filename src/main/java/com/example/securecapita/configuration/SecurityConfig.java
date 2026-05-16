ef328a4c5edb00de86191c73aaa2d21a41df312b
Ajay <ajaynimmala18@gmail.com>
2026-05-16 22:31:41 +0530

    Implementing SecurityFilterChain

--- src/main/java/com/example/securecapita/configuration/SecurityConfig.java
+++ src/main/java/com/example/securecapita/configuration/SecurityConfig.java
@@ -9,13 +9,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@RequiredArgsConstructor
 @Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig{
 
     private  final BCryptPasswordEncoder encoder;
